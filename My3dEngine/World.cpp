24320825bed0ad135f84bff805af6c561da88f0f
Chen Liang <liangchen0322@gmail.com>
2016-09-16 09:09:25 +0800

    World basic test
    Signed-off-by: Chen Liang <liangchen0322@gmail.com>

---
+++ My3dEngine/World.cpp
@@ -0,0 +1,42 @@
+#include "stdafx.h"
+#include "World.h"
+#include <iostream>
+
+static int count = 0;
+
+g3::World::World(unsigned int w, unsigned int h):
+  width {w},
+  height {h}
+{
+	depthBuffer.reset(new float[width * height]);
+	frontBuffer = (COLORREF*)malloc(width * height * sizeof(COLORREF));
+}
+
+/**
+ * Clears the buffers.
+ */
+void g3::World::clear()
+{
+	// Fill the buffer with color white
+	memset((void *)frontBuffer, 0xDD, width * height * sizeof(COLORREF));
+	// Clears the depth buffer
+	std::fill(depthBuffer.get(), depthBuffer.get() + (width*height), std::numeric_limits<float>::infinity());
+}
+
+void g3::World::freshFrame(void **buffer)
+{
+	clear();
+	COLORREF color = RGB(0, 0, 0);
+	int t = count % 300;
+	drawLine(t, 0, 200 + t, 200, color);
+	count += 10;
+
+	*buffer = frontBuffer;
+}
+
+void g3::World::drawLine(int x0, int y0, int x1, int y1, COLORREF c1)
+{
+	for (int i = x0; i < x1; i++) {
+		frontBuffer[i * width + i] = c1;
+	}
+}
---
+++ My3dEngine/World.h
@@ -0,0 +1,35 @@
+#ifndef _WORLD_H_
+#define _WORLD_H_
+
+#include <Windows.h>
+#include <memory>
+#include <limits>
+
+namespace g3 {
+
+class World {
+public:
+	World(unsigned int w, unsigned int h);
+	void freshFrame(void **buffer);
+
+private:
+	// void drawPoint(int x, int y, float z, COLORREF color);
+	// void drawLine(int x0, int y0, float z0, COLORREF c0, int x1, int y1, float z1, COLORREF c1);
+	void drawLine(int x0, int y0, int x1, int y1, COLORREF c1);
+	void clear(void);
+
+	/* The width of the screen. */
+	unsigned int width;
+	unsigned int height;
+
+	COLORREF *frontBuffer;
+	std::unique_ptr<float[]> depthBuffer;
+
+	unsigned long targetFrameTime;
+	unsigned long startFrameTime;
+	unsigned long finishFrameTime;
+};
+
+}
+
+#endif
